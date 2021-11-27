014c0e6848033476b65143140c035a2745df7581
mrehanali127 <mrehanali127@gmail.com>
2021-11-27 09:48:36 +0500

    Setup Project Structure

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore,combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import productsReducer from './store/reducers/products';
+
+const rootReducer=combineReducers({
+  products:productsReducer
+});
+
+const store=createStore(rootReducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Helllo Deaar</Text>
-      <StatusBar style="auto" />
-    </View>
+   <Provider store={store}>
+     <View>... </View>
+   </Provider>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
